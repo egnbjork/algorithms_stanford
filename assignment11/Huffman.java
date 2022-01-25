62bf0186cbb0da323dff374e84d60e82376f4122
Yevgen Berberyan <egnbjork@gmail.com>
2022-01-25 23:37:17 +0200

    huffman half done

--- assignment11/Huffman.java
+++ assignment11/Huffman.java
@@ -11,6 +11,22 @@ public class Huffman {
 
     PriorityQueue<HuffmanObject> pq = readFile(fileName);
     System.out.println(pq);
+
+    PriorityQueue<HuffmanObject> list = calc(pq);
+    System.out.println(list);
+  }
+
+  private static PriorityQueue<HuffmanObject> calc(PriorityQueue<HuffmanObject> pq) {
+    while(pq.size() > 2) {
+      System.out.println(pq);
+
+      HuffmanObject o1 = pq.poll();
+      HuffmanObject o2 = pq.poll();
+      System.out.println("merge " + o1.getWeight() + " and " + o2.getWeight());
+      o2.merge(o1);
+      pq.add(o2);
+    }
+    return pq;
   }
 
   private static PriorityQueue<HuffmanObject> readFile(String fileName) {
@@ -42,6 +58,20 @@ public class Huffman {
 
     HuffmanObject(Integer weight) {
       this.weight = weight;
+      children = new ArrayList<>();
+    }
+
+    void merge(HuffmanObject o1) {
+      HuffmanObject o2 = new HuffmanObject(weight);
+      o2.children = new ArrayList<>(children);;
+      List<HuffmanObject> mergedChildren = Arrays.asList(o1, o2);
+      this.children = mergedChildren;
+
+      if(children.size() > 2) {
+        throw new IllegalArgumentException("children number exceeded");
+      }
+
+      this.weight = o1.getWeight() + o2.getWeight();
     }
 
     @Override
@@ -51,7 +81,7 @@ public class Huffman {
 
     @Override
     public String toString() {
-      return weight.toString();
+      return weight.toString() + (children.isEmpty() ? "" : children.toString());
     }
 
     Integer getWeight() {
