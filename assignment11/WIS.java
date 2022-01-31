0845ea59a7b3277bf913330ee95c99cd7ed13f3b
Yevgen Berberyan <egnbjork@gmail.com>
2022-01-31 14:15:22 +0200

    wis incorrect answer

--- assignment11/WIS.java
+++ assignment11/WIS.java
@@ -9,8 +9,49 @@ public class WIS {
     }
     String fileName = args[0];
 
-    List<Integer> list = readFile(fileName);
-    System.out.println(list);
+    List<Integer> weightList = readFile(fileName);
+    System.out.println(weightList.size());
+
+    Set<Integer> indexSet = calc(weightList);
+    System.out.println(indexSet);
+
+    //checkIfExists(indexSet, Arrays.asList(1, 2, 3, 4, 17, 117, 517, 997));
+    checkIfExists(indexSet, Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+  }
+
+  private static void checkIfExists(Set<Integer> indexSet, List<Integer> checkList) {
+    checkList.forEach(n -> {
+      if(indexSet.contains(n)) {
+        System.out.print("1");
+      } else {
+        System.out.print("0");
+      }
+    });
+    System.out.println("");
+  }
+
+  private static Set<Integer> calc(List<Integer> weightList) {
+    int sum = 0;
+    Set<Integer> indexSet = new TreeSet<>();
+
+    int index = weightList.size() - 1;
+    while(index >= 0) {
+      System.out.println("index: " + index);
+      int a1 = index - 1 >= 0 ? weightList.get(index - 1) : 0;
+      int a2 = index - 2 >= 0 ? weightList.get(index - 2) + weightList.get(index) : weightList.get(index);
+      if(a1 >= a2) {
+        index -= 1;
+      } else {
+        sum += weightList.get(index);
+        indexSet.add(index);
+        index -= 2;
+      }
+      System.out.println("sum: " + sum);
+      System.out.println("indexSet: " + indexSet);
+    }
+     
+    System.out.println("sum is " + sum);
+    return indexSet;
   }
 
   private static List<Integer> readFile(String fileName) {
@@ -22,6 +63,7 @@ public class WIS {
       currentLine = reader.readLine();
       System.out.println(currentLine + " weights in the file");
 
+      list.add(0);
       do {
         currentLine = reader.readLine();
         if(currentLine == null) break;
