c95f8c9cc1b2956edde0f93db2588186d857903c
Yevgen Berberyan <egnbjork@gmail.com>
2023-09-23 14:09:58 +0200

    read from file

--- assignment14/HeldKarp.java
+++ assignment14/HeldKarp.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class HeldKarp {
   //add first city to hashtable with coordinates
@@ -7,9 +8,25 @@ public class HeldKarp {
   //add third city to hashtable with coordinates
   //add third city to every entry in hashtable with distances + swap to get all permutations (ABC ACB CAB)
 
-  private static String string = "ABC";
-
   public static void main(String[] args) {
-    System.out.println(string);
+        if (args.length < 1) {
+            System.out.println("Please provide file name");
+            return;
+        }
+        List<String> citiesCoords = readFile(args[0]);
+        System.out.println(citiesCoords);
   }
+
+    private static List<String> readFile(String filename) {
+      List<String> coords = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line = br.readLine();
+            while ((line = br.readLine()) != null) {
+              coords.add(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return coords;
+    }
 }
